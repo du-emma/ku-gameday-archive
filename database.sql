f96b535993b590e72a82d1febc1602169e052793
Emma Du <du.emma@ku.edu>
2026-04-27 17:37:51 -0500

    added more player stats for Oklahoma game

--- database.sql
+++ database.sql
@@ -72,15 +72,27 @@ INSERT INTO Games (gameDate, season, opponent, location, KUscore, opponentScore,
 
 INSERT INTO Players (name, number, year, position, classYear) VALUES
 ('Hunter Dickinson', 1, 2024, 'Center', 'Senior'),
-('Dajuan Harris Jr.', 3, 2024, 'Guard', 'Senior'),
+('Kevin McCullar Jr.', 15, 2024, 'Guard', 'Senior'),
 ('KJ Adams Jr.', 24, 2024, 'Forward', 'Junior'),
-('Kevin McCullar Jr.', 15, 2024, 'Guard', 'Senior');
+('Dajuan Harris Jr.', 3, 2024, 'Guard', 'Senior'),
+('Johnny Furphy', 10, 2024, 'Guard', 'Freshman'),
+('Elmarko Jackson', 13, 2024, 'Guard', 'Freshman'),
+('Parker Braun', 23, 2024, 'Forward', 'Senior'),
+('Nicolas Timberlake', 25, 2024, 'Guard', 'Senior'),
+('Jamari McDowell', 11, 2024, 'Guard', 'Freshman');
+
 
 INSERT INTO PlayerStats (gameID, playerID, points, rebounds, assists, steals, blocks, minutesPlayed) VALUES
-(1, 1, 24, 14, 2, 1, 2, 34),
-(1, 2, 9, 3, 8, 2, 0, 35),
-(1, 3, 15, 6, 3, 1, 1, 32),
-(1, 4, 18, 5, 4, 2, 0, 36),
+(1, 1, 24, 14, 2, 0, 5, 33),   
+(1, 2, 21, 4, 4, 1, 0, 39),  
+(1, 3, 15, 10, 3, 1, 1, 40),   
+(1, 4, 7, 1, 8, 3, 0, 40),    
+(1, 5, 7, 3, 0, 2, 0, 19),    
+(1, 6, 2, 2, 0, 0, 1, 18),     
+(1, 7, 2, 1, 0, 0, 0, 7),    
+(1, 8, 0, 0, 0, 0, 0, 3),    
+(1, 9, 0, 0, 0, 0, 0, 1);     
+
 
 (2, 1, 19, 12, 1, 0, 1, 33),
 (2, 2, 11, 2, 7, 1, 0, 34),
